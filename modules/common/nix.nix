9f2380c8096d513698ff07793834f77822f02d20
ByronLi8565 <byronli8565@gmail.com>
2026-05-18 11:43:43 -0500

    Fix determinate

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -1,18 +1,5 @@
-{pkgs, ...}: {
-  nix.package = pkgs.nix;
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-    substituters = [
-      "https://cache.nixos.org/"
-      "https://jrestivo.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "jrestivo.cachix.org-1:+jSOsXAAOEjs+DLkybZGQEEIbPG7gsKW1hPwseu03OE="
-    ];
-    trusted-users = [
-      "root"
-      "spheal"
-    ];
-  };
+{
+  # Determinate Nix manages the Nix daemon. Keep nix-darwin from trying to
+  # activate or overwrite daemon-level Nix configuration.
+  nix.enable = false;
 }
